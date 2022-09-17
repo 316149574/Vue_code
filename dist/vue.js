c2058857c0c3c7c272bcb25b23af346ad2f99f4b
duanli <316149574@qq.com>
2022-09-17 15:50:38 +0800

    ast

--- dist/vue.js
+++ dist/vue.js
@@ -4,12 +4,110 @@
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Vue = factory());
 })(this, (function () { 'use strict';
 
+    var ncname = "[a-zA-Z_][\\-\\.0-9_a-zA-Z]*"; // 标签名 
+
+    var qnameCapture = "((?:".concat(ncname, "\\:)?").concat(ncname, ")"); // aa:xx 命令空间  用来获取标签名  match后索引为1的为匹配到的标签名   let r =  '<div></div>'.match(qnameCapture);
+
+    var startTagOpen = new RegExp("^<".concat(qnameCapture)); // 匹配开始标签
+
+    var startTagClose = /^\s*(\/?)>/; //   />  <div/> 
     //  匹配属性  a=b  a="b"  a='b' 三种情况      aa  =  “  XXX ” | ‘  XXX  ’  |  XXX
-    "[a-zA-Z_][\\-\\.0-9_a-zA-Z".concat(unicodeRegExp.source, "]*"); // 标签名 
-    //  html标签解析成ast树  
+
+    var attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/;
+    var endTag = new RegExp("^<\\/".concat(qnameCapture, "[^>]*>")); // 匹配闭合标签 </xxxx   >
+    //  html标签解析成DOM树 <div id="app"> {{name}}</div>   
 
     function compileToFunction(template) {
+      parserHTML(template);
     } // htmlparser2 第三方插件与parserHTML一样  把html转化成ast语法树
+
+    function parserHTML(html) {
+      //  前进删除以匹配的内容
+      function advance(len) {
+        html = html.substring(len);
+      }
+
+      function paserStartTag() {
+        var start = html.match(startTagOpen); //console.log(start)==>  ['<div', 'div', index: 0, input: '<div id="app"> {{name}}</div>', groups: undefined]
+
+        if (start) {
+          var match = {
+            tagName: start[1],
+            attrs: []
+          };
+          advance(start[0].length); //  id="app"> {{name}}</div>
+          // 匹配属性
+
+          var _end;
+
+          var attr;
+
+          while (!(_end = html.match(startTagClose)) && (attr = html.match(attribute))) {
+            // 没到开始标签闭合 ">" 则说明有n个属性
+            // attr ==>  [' id="app"', 'id', '=', 'app', undefined, undefined, index: 0, input: ' id="app" class="div1">{{name}}</div>', groups: undefined] 
+            match.attrs.push({
+              name: attr[1],
+              value: attr[3] || attr[4] || attr[5]
+            });
+            advance(attr[0].length);
+          }
+
+          if (_end) {
+            advance(_end[0].length);
+          }
+
+          return match;
+        }
+
+        return false; // 不是开始标签
+      }
+
+      while (html) {
+        var textend = html.indexOf('<'); // 当前解析的开头<div id="app">{{name}}</div>
+
+        if (textend == 0) {
+          var startTagMatch = paserStartTag(); // 如果是开始标签
+
+          if (startTagMatch) {
+            start(startTagMatch.tagName, startTagMatch.attrs);
+            continue;
+          } // 如果是结束标签
+
+
+          var endTagMatch = html.match(endTag);
+
+          if (endTagMatch) {
+            end(endTagMatch[1]);
+            advance(endTagMatch[0].length);
+            continue;
+          }
+        }
+
+        var text = void 0;
+
+        if (textend > 0) {
+          //  {{name}}</div>
+          text = html.substring(0, textend);
+        }
+
+        if (text) {
+          chars(text);
+          advance(text.length); // </div>
+        }
+      }
+    }
+
+    function start(tagName, attributes) {
+      console.log(tagName, attributes);
+    }
+
+    function chars(text) {
+      console.log(text);
+    }
+
+    function end(tagName) {
+      console.log(tagName);
+    }
 
     function _typeof(obj) {
       "@babel/helpers - typeof";
@@ -228,7 +326,7 @@
 
           if (!template && el) {
             template = el.outerHTML;
-            var render = compileToFunction();
+            var render = compileToFunction(template);
             options.render = render; // options.render函数就是渲染函数
           }
         }
